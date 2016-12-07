1582d8a39084d5699c3692570ab424fa7d762977
zhaogaoxing <865072432@qq.com>
2016-12-07 09:14:06 +0800

    revise Character creation interface

---
+++ 角色创造界面/clientFight/temp/scripts/assets/serverSceneSrc/script/myServer_02.js
@@ -0,0 +1,34 @@
+"use strict";
+cc._RFpush(module, '3828b3k1JREXJMCmk2w5WnB', 'myServer_02');
+// serverSceneSrc\script\myServer_02.js
+
+cc.Class({
+    'extends': cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,      // The default value will be used only when the component attaching
+        //                           to a node for the first time
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+    },
+
+    // use this for initialization
+    onLoad: function onLoad() {
+        this.node.on('mousedown', function () {
+            cc.director.loadScene('heroChooseScene');
+        });
+    }
+
+});
+// called every frame, uncomment this function to activate update callback
+// update: function (dt) {
+
+// },
+
+cc._RFpop();
